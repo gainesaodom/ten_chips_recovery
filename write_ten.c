b82d99d49ef99109bbac7b9af64c459c05131e93
gainesaodom <136353693+gainesaodom@users.noreply.github.com>
2024-11-05 14:11:48 -0600

    Update write_ten.c

--- write_ten.c
+++ write_ten.c
@@ -1,6 +1,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "spi23x1024.c"
+#include <wiringPi.h>
+
+void change_select_pin(int chip_num) 
+{
+	for (int i = 0; i < sizeof(select_line_pin); i++) 
+	{
+        int bit = (chip_num >> i) & 1; // Extract the i-th bit of chip_num
+        digitalWrite(select_line_pin[i], bit); // Set the pin based on the extracted bit value	
+	}
+}
 
 uint16_t img_write() {
 
@@ -30,7 +40,7 @@ uint16_t img_write() {
         // Parse the "Address" and "Byte" data
         unsigned int address;
         unsigned int byte;
-        if (sscanf(buffer, "%4x,%2x", &address, &byte) != 2) {
+        if (sscanf(buffer, "%5x,%2x", &address, &byte) != 2) {
             perror("Error parsing data");
             fclose(file);
             return 1;
@@ -42,14 +52,20 @@ uint16_t img_write() {
 
     fclose(file);
 
-    spi_mem_init(5000000);
-	uint16_t address_idx;
-	for (address_idx = 0; address_idx <= SPI_MEM_MAX_ADDRESS; address_idx++) {
-		//printf("%x\n", address_idx);
-		uint8_t next_value = csv_data[address_idx];
-		spi_mem_write_byte(address_idx, next_value);
+	int chip_num;
+	for (chip_num = 1; chip_num <= 10; chip_num++)
+	{
+		change_select_pin(chip_num);
+		spi_mem_init(5000000);
+		
+		int address_idx;
+		for (address_idx = 0; address_idx <= SPI_MEM_MAX_ADDRESS; address_idx++) {
+			//printf("%x\n", address_idx);
+			uint8_t next_value = csv_data[address_idx];
+			spi_mem_write_byte(address_idx, next_value);
+		}
+		spi_mem_close();
 	}	
-    spi_mem_close();
     printf("Done Writing Image to Chips.\n");
     return 0;
 }
